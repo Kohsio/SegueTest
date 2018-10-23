845069097a2a8b72f9aaa35e07e7f3561fb8ec5d
Kohsio <nguyen_minhduy@hotmail.com>
2018-10-23 01:13:32 -0700

    Segue

--- SegueTest/ViewController.swift
+++ SegueTest/ViewController.swift
@@ -14,12 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    @IBOutlet weak var switchColor: UISwitch!
+    
+    @IBAction func Yellow(_ sender: Any) {
+        if switchColor.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+        
+    }
+    @IBAction func greenButton(_ sender: Any) {
+        if switchColor.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
 }
 
